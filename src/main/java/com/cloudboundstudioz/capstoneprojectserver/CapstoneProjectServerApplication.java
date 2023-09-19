a4e233c7f7a919ab3bf97c98ee4a53edf45b5364
Student 4 <student4@Codes-MacBook-Air-87.local>
2023-09-19 15:54:56 -0400

    initial commit

---
+++ src/main/java/com/cloudboundstudioz/capstoneprojectserver/CapstoneProjectServerApplication.java
@@ -0,0 +1,13 @@
+package com.cloudboundstudioz.capstoneprojectserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneProjectServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapstoneProjectServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cloudboundstudioz/capstoneprojectserver/CapstoneProjectServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudboundstudioz.capstoneprojectserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneProjectServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
