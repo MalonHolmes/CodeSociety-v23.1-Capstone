ca4a9d5b9fb9e9c6866fc2497af045d54a496c57
Student 4 <student4@Codes-MacBook-Air-87.local>
2023-09-24 23:32:31 -0400

    Completed User Service Tests, started some User Controller Tests

--- src/test/java/com/cloudboundstudioz/capstoneprojectserver/domain/user/UserControllerTest.java
+++ src/test/java/com/cloudboundstudioz/capstoneprojectserver/domain/user/UserControllerTest.java
@@ -1,4 +1,87 @@
 package com.cloudboundstudioz.capstoneprojectserver.domain.user;
 
+import com.cloudboundstudioz.capstoneprojectserver.domain.core.exceptions.ResourceCreationException;
+import com.cloudboundstudioz.capstoneprojectserver.domain.core.exceptions.ResourceNotFoundException;
+import com.cloudboundstudioz.capstoneprojectserver.domain.user.controllers.UserController;
+import com.cloudboundstudioz.capstoneprojectserver.domain.user.models.UserModel;
+import com.cloudboundstudioz.capstoneprojectserver.domain.user.services.UserService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
 public class UserControllerTest {
+
+    @MockBean
+    private UserService userService;
+
+    private UserController testController;
+    private UserModel testUser;
+
+    @BeforeEach
+    public void setup(){
+        testController = new UserController(userService);
+        UserModel testUser = new UserModel();
+    }
+
+    @Test
+    @DisplayName("User Controller: Create User - Success")
+    void createUserTest01() {
+
+        when(userService.create(any())).thenReturn(testUser);
+
+        ResponseEntity<UserModel> response = testController.create(testUser);
+
+        assertEquals(HttpStatus.CREATED, response.getStatusCode());
+
+    }
+
+    @Test
+    @DisplayName("User Controller: Create User - Fail")
+    void testCreateFails() {
+
+        when(userService.create(any()))
+                .thenThrow(new ResourceCreationException("Create failed"));
+
+        assertThrows(RuntimeException.class, () -> {
+            testController.create(new UserModel());
+        });
+
+    }
+
+    @Test
+    @DisplayName("User Controller: Get User By Id - Success")
+    void getUserByIdTest01() {
+
+        UserModel mockUser = new UserModel();
+
+        when(userService.getById(1L)).thenReturn(mockUser);
+
+        ResponseEntity<UserModel> response = testController.getById(1L);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+
+    }
+
+    @Test
+    @DisplayName("User Controller: Get User By Id - Fail")
+    void getUserByIdTest02() {
+
+        when(userService.getById(1L))
+                .thenThrow(new ResourceNotFoundException("User not found!"));
+
+        assertThrows(ResourceNotFoundException.class, () -> {
+            testController.getById(1L);
+        });
+
+    }
 }
--- src/test/java/com/cloudboundstudioz/capstoneprojectserver/domain/user/UserServiceTest.java
+++ src/test/java/com/cloudboundstudioz/capstoneprojectserver/domain/user/UserServiceTest.java
@@ -1,6 +1,7 @@
 package com.cloudboundstudioz.capstoneprojectserver.domain.user;
 
 import com.cloudboundstudioz.capstoneprojectserver.domain.core.exceptions.ResourceCreationException;
+import com.cloudboundstudioz.capstoneprojectserver.domain.core.exceptions.ResourceNotFoundException;
 import com.cloudboundstudioz.capstoneprojectserver.domain.profile.models.Profile;
 import com.cloudboundstudioz.capstoneprojectserver.domain.user.models.UserModel;
 import com.cloudboundstudioz.capstoneprojectserver.domain.user.repositories.UserRepo;
@@ -11,14 +12,16 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentMatchers;
 import org.mockito.BDDMockito;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
+import java.util.*;
+
 import static org.assertj.core.api.BDDAssertions.fail;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 @SpringBootTest
 public class UserServiceTest {
@@ -33,9 +36,9 @@ public class UserServiceTest {
     private UserRepo testRepo;
 
     // test user objects
-    private UserModel testUser1;
-    private UserModel testUserExpected;
-    private UserModel testUserActual;
+    private UserModel mockUser1;
+    private UserModel mockUserExpected;
+    private UserModel mockUserActual;
 
     // test profile
     private Profile testProfile;
@@ -43,19 +46,20 @@ public class UserServiceTest {
     @BeforeEach
     public void setup() {
         testProfile = new Profile("Lonny Love", 23, 68.5, 145.2);
-        testUser1 = new UserModel("CloudBound", "cloud@bound.com", "clbd", testProfile);
-        testUserExpected = testUser1;
-        testUserActual = new UserModel();
+        mockUser1 = new UserModel("CloudBound", "cloud@bound.com", "clbd", testProfile);
+        mockUserExpected = mockUser1;
+        mockUserActual = new UserModel();
     }
 
+    // Create Tests
     @Test
     @DisplayName("User Service: Create User - Success")
     public void createUserTest01(){ // When I try to create a user, that exact user is successfully created
 
-        BDDMockito.doReturn(testUserExpected).when(testRepo).save(ArgumentMatchers.any());
-        testUserActual = new UserModel("CloudBound", "cloud@bound.com", "clbd", testProfile);
-        assertNotNull(testUserActual);
-        assertEquals(testUserExpected, testUserActual);
+        BDDMockito.doReturn(mockUserExpected).when(testRepo).save(ArgumentMatchers.any());
+        mockUserActual = new UserModel("CloudBound", "cloud@bound.com", "clbd", testProfile);
+        assertNotNull(mockUserActual);
+        assertEquals(mockUserExpected, mockUserActual);
     }
 
     @Test
@@ -67,10 +71,183 @@ public class UserServiceTest {
 
         // Act & Assert
         try {
-            testService.create(testUser1);
+            testService.create(mockUser1);
             fail("Expected ResourceCreationException but it wasn't thrown");
         } catch (ResourceCreationException e) {
             assertEquals("User Creation Failed!", e.getMessage());
         }
     }
+
+    // Get Tests
+    @Test
+    @DisplayName("User Service: Get User By Id - Success")
+    public void getUserByIdTest01() {
+        // Arrange: mock create user and mock repo
+        mockUser1.setId(1L);
+        BDDMockito.when(testRepo.findById(1L))
+                .thenReturn(Optional.of(mockUser1));
+
+        // Act: Call service method
+        UserModel result = testService.getById(1L);
+
+        // Assert: Verify result
+        assertNotNull(result);
+        assertEquals(1L, result.getId());
+    }
+
+    @Test
+    @DisplayName("User Service: Get User By Id - Fail")
+    public void getUserByIdTest02(){
+        // Arrange: Mock findById to return empty
+        BDDMockito.when(testRepo.findById(1L))
+                .thenReturn(Optional.empty());
+
+        // Act & Assert: Call service, assert exception
+        assertThrows(ResourceNotFoundException.class, () -> {
+            testService.getById(1L);
+        });
+    }
+
+    @Test
+    @DisplayName("User Service: Get User By Username - Success")
+    public void getUserByUsernameTest01() {
+        // Arrange: mock user
+        BDDMockito.when(testRepo.findByUsername(mockUser1.getUsername()))
+                .thenReturn(Optional.of(mockUser1));
+
+        // Act: Call service method
+        UserModel result = testService.getByUsername("CloudBound");
+
+        // Assert: not null and email
+        assertNotNull(result);
+        assertEquals("CloudBound", result.getUsername());
+    }
+
+    @Test
+    @DisplayName("User Service: Get User By Username - Fail")
+    public void getUserByUsernameTest02(){
+        // Arrange: Mock findByEmail to return empty
+        BDDMockito.when(testRepo.findByUsername(mockUser1.getUsername()))
+                .thenReturn(Optional.empty());
+
+        // Act & Assert: Call service, assert exception
+        assertThrows(ResourceNotFoundException.class, () -> {
+            testService.getByUsername("CloudBound");
+        });
+    }
+
+    @Test
+    @DisplayName("User Service: Get User By Email - Success")
+    public void getUserByEmailTest01() {
+        // Arrange: mock user
+        BDDMockito.when(testRepo.findByEmail(mockUser1.getEmail()))
+                .thenReturn(Optional.of(mockUser1));
+
+        // Act: Call service method
+        UserModel result = testService.getByEmail("cloud@bound.com");
+
+        // Assert: not null and email
+        assertNotNull(result);
+        assertEquals("cloud@bound.com", result.getEmail());
+    }
+
+    @Test
+    @DisplayName("User Service: Get User By Email - Fail")
+    public void getUserByEmailTest02(){
+        // Arrange: Mock findByEmail to return empty
+        BDDMockito.when(testRepo.findByEmail(mockUser1.getEmail()))
+                .thenReturn(Optional.empty());
+
+        // Act & Assert: Call service, assert exception
+        assertThrows(ResourceNotFoundException.class, () -> {
+            testService.getByEmail("cloud@bound.com");
+        });
+    }
+
+    @Test
+    @DisplayName("User Service: Get All Users - Success")
+    public void getAllTest01(){
+        // Arrange: create list of mock users and mock it in repo
+        List<UserModel> mocks = new ArrayList<>();
+        mocks.add(mockUser1);
+        mocks.add(mockUser1);
+        mocks.add(mockUser1);
+
+        BDDMockito.when(testRepo.findAll()).thenReturn(mocks); // return mock list when repo calls findAll()
+
+        // Act: call getAll() method
+        List<UserModel> actual = testService.getAll();
+
+        // Assert: list length is the same
+        assertEquals(mocks.size(), actual.size());
+    }
+
+    @Test
+    @DisplayName("User Service: Get All Users - Fail")
+    public void getAllTest02(){
+        // Arrange: mock throwing an error when calling findAll
+        BDDMockito.when(testRepo.findAll()).thenThrow(new ResourceNotFoundException("Failed to retrieve users"));
+
+        // Act & Assert: Calling getAll() method throws Resource Not Found Exception
+        assertThrows(ResourceNotFoundException.class, () -> {
+            testService.getAll();
+        });
+    }
+
+    // Update Tests
+    @Test
+    @DisplayName("User Service: Update User - Success")
+    public void updateUserTest01(){
+        // Arrange: create an updated user and mock it in the db
+        mockUser1.setId(1L);
+        BDDMockito.when(testRepo.save(mockUser1)).thenReturn(mockUser1);
+
+        UserModel updatedUser =
+                new UserModel("CloudBound_Update",
+                        "update@update.com",
+                        "update",
+                        testProfile);
+        updatedUser.setId(1L);
+
+        BDDMockito.when(testRepo.save(updatedUser)).thenReturn(updatedUser);
+
+        // Act: update the user
+        UserModel result = testService.updateUser(1L, updatedUser);
+
+        // Assert every field matches
+        assertEquals(updatedUser.getId(), result.getId());
+        assertEquals(updatedUser.getUsername(), result.getUsername());
+        assertEquals(updatedUser.getEmail(), result.getEmail());
+        assertEquals(updatedUser.getPassword(), result.getPassword());
+        assertEquals(updatedUser.getProfile(), result.getProfile());
+    }
+
+    // Delete Tests
+
+    @Test
+    @DisplayName("User Service: Delete User - Success")
+    public void deleteUserTest01(){
+        // Arrange: create user to be deleted, mock it to db
+        mockUser1.setId(1L);
+        BDDMockito.when(testRepo.save(mockUser1)).thenReturn(mockUser1);
+
+        // Act: delete user
+        testService.deleteUser(1L);
+
+        // Assert
+        verify(testRepo, times(1)).deleteById(1L);
+    }
+
+    @Test
+    public void deleteUserTest02(){
+        // Arrange
+        BDDMockito.when(testRepo.findById(1L))
+                .thenReturn(Optional.empty());
+
+        // Act and Assert
+        assertThrows(ResourceNotFoundException.class, () -> {
+            testService.deleteUser(1L);
+        });
+    }
+
 }
