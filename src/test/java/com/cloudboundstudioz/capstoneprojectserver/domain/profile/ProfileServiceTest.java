128a3715f395b99012e1ed93e81e33b6b9fc2662
Student 4 <student4@Codes-MacBook-Air-87.local>
2023-09-28 23:47:06 -0400

    Worked on Profile Service Tests - not done yet

--- src/test/java/com/cloudboundstudioz/capstoneprojectserver/domain/profile/ProfileServiceTest.java
+++ src/test/java/com/cloudboundstudioz/capstoneprojectserver/domain/profile/ProfileServiceTest.java
@@ -1,48 +1,104 @@
 package com.cloudboundstudioz.capstoneprojectserver.domain.profile;
 
-import com.cloudboundstudioz.capstoneprojectserver.CapstoneProjectServerApplication;
+import com.cloudboundstudioz.capstoneprojectserver.domain.core.exceptions.ResourceCreationException;
+import com.cloudboundstudioz.capstoneprojectserver.domain.core.exceptions.ResourceNotFoundException;
 import com.cloudboundstudioz.capstoneprojectserver.domain.profile.models.Profile;
 import com.cloudboundstudioz.capstoneprojectserver.domain.profile.repositories.ProfileRepo;
 import com.cloudboundstudioz.capstoneprojectserver.domain.profile.services.ProfileService;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentMatchers;
 import org.mockito.BDDMockito;
-import org.mockito.InjectMocks;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
 public class ProfileServiceTest {
 
     @Autowired
-    private ProfileService profileService;
+    private ProfileService testService;
 
     @MockBean
-    private ProfileRepo profileRepo;
+    private ProfileRepo testRepo;
 
-    private Profile testProfile;
-    private Profile returnedProfile;
+    private Profile testProfile1;
+    private Profile testProfile2;
     @BeforeEach
     public void setup() {
-        testProfile = new Profile("Lonny Love", 23, 68.5, 145.2);
-        returnedProfile = new Profile();
+        testProfile1 = new Profile("Lonny Love", 23, 68.5, 145.2);
+        testProfile2 = new Profile();
     }
 
     @Test
+    @DisplayName("Profile Service: Create Profile - Success")
     public void createProfileTest(){
-        BDDMockito.doReturn(returnedProfile).when(profileRepo).save(ArgumentMatchers.any());
-        Profile newProfile = profileService.create(new Profile("Miles Morales", 17, 58.4, 130.2));
-        newProfile.setId(1L);
-        assertNotNull(newProfile);
-        assertEquals(1, newProfile.getId());
+
+        // Arrange: mock repo create method to return the created profile
+        BDDMockito.when(testRepo.save(testProfile1)).thenReturn(testProfile1);
+
+        // Act: Call service create method
+        Profile result = testService.create(testProfile1);
+
+        // Assert: new profile is created and matches what we passed
+        assertEquals(testProfile1, result);
+    }
+
+    @Test
+    @DisplayName("Profile Service: Create Profile - Fail")
+    public void createUserTest02(){
+        // Arrange
+        BDDMockito.doThrow(new ResourceCreationException("Profile Creation Failed!"))
+                .when(testRepo).save(ArgumentMatchers.any());
+
+        // Act & Assert: call service create(), assert resource creation exception to be thrown
+        assertThrows(ResourceCreationException.class, () -> {
+            testService.create(testProfile1);
+        });
+    }
+
+    @Test
+    @DisplayName("Profile Service: Get Profile By Id - Success")
+    public void getProfileByIdTest01() {
+        // Arrange: mock create user and mock repo
+        testProfile1.setId(1L);
+        BDDMockito.when(testRepo.findById(1L))
+                .thenReturn(Optional.of(testProfile1));
+
+        // Act: Call service getbyid method
+        Profile result = testService.getById(1L);
+
+        // Assert: Verify result
+        assertEquals(testProfile1.getId(), result.getId());
+        assertEquals(testProfile1, result);
+    }
+
+    @Test
+    @DisplayName("Profile Service: Get Profile By Id - Fail")
+    public void getProfileByIdTest02(){
+        // Arrange: Mock findById to throw exception
+        BDDMockito.when(testRepo.findById(1L)).thenThrow(new ResourceNotFoundException("Profile Not Found!"));
+
+        // Act & Assert: Call service, assert exception
+        assertThrows(ResourceNotFoundException.class, () -> {
+            testService.getById(1L);
+        });
+    }
+
+    @Test
+    @DisplayName("Profile Service: Get Profile By Name - Success")
+    public void getProfileByNameTest01() {
+        // Arrange
+        // Act
+        // Assert
     }
 
 
